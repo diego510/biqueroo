e490ac7ab42470d4a98058027a2f0826c17c1d4e
Valechv <valeria.champac@utec.edu.pe>
2023-11-21 11:54:54 -0500

    cross origin para publication

--- src/main/java/com/example/biquerito/controllers/PublicationController.java
+++ src/main/java/com/example/biquerito/controllers/PublicationController.java
@@ -3,12 +3,7 @@ package com.example.biquerito.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.security.access.annotation.Secured;
 
@@ -22,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/publication")
+@CrossOrigin
 public class PublicationController {
 
     @Autowired
